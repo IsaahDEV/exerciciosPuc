2798f59830536a581e355c855ddea205749b4acc
IsaahDEV <isabelesilvaah16@gmail.com>
2023-10-07 21:43:11 -0300

    realizei exercicio novo

---
+++ execicio c#/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int nota;
+Console.Write("Digite a nota do aluno;");
+nota = int.Parse(Console.ReadLine());
+if(nota >= 60)
+Console.WriteLine("O aluno foi aprovado");
